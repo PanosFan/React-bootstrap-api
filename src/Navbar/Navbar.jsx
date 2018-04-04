a9ffa559fb397f8116b1eb7733a61992174fe6a8
Panagiotis Fanartzis <eleinos100@hotmail.com>
2018-04-04 14:18:00 +0300

    removed jquery from navbar, using state for my toggler instead

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
 import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
-import $ from 'jquery';
+// import $ from 'jquery';
 import './Navbar.css';
 
 class Navbar extends Component {
+
+	state = {
+		toggler : false
+	}
+
+	handleclick = () => {
+		this.setState ({
+			toggler : !this.state.toggler
+		});
+	}
+
+
+
+
+
+
+
 	render () {
 		return (
 			<div className="Navbar">
@@ -12,8 +29,8 @@ class Navbar extends Component {
 
 				    <span className="navbar-brand">Brand-Name</span>
 
-				    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				      <span className="navbar-toggler-icon">
+				    <button onClick={this.handleclick} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				      <span className={this.state.toggler? "navbar-toggler-icon on" : "navbar-toggler-icon"}> 
 				       	<div className="span" id="one"></div>
 				        <div className="span" id="two"></div>
 				       	<div className="span" id="three"></div>
@@ -46,13 +63,11 @@ class Navbar extends Component {
 
 
 
-$(function(){
-      
-	$(".navbar-toggler").on("click",function (){
-    	$(".navbar-toggler-icon").toggleClass('on');
-    });
-
-});
+// $(function(){      
+// 	$(".navbar-toggler").on("click",function (){
+//     	$(".navbar-toggler-icon").toggleClass('on');
+//     });
+// });
 
             
 
